b404e76e2ab2120617984d783cf0df19659e580c
IVanov-TA <qnko.vanov.ta@gmail.com>
2014-06-01 20:12:06 +0300

    [QCode] High Quality Classes homework
    all task form current chapter

---
+++ C Sharp QualityCode/[QCode] HighQualityClasses/Cohesion-and-Coupling/Point.cs
@@ -0,0 +1,40 @@
+﻿namespace CohesionAndCoupling
+{
+    internal struct Point
+    {
+        private double x, y;
+
+        public Point(double x, double y)
+            : this()
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public double X 
+        { 
+            get
+            {
+                return this.x;
+            }
+
+            set 
+            {
+                this.x = value;
+            }
+        }
+
+        public double Y
+        {
+            get
+            {
+                return this.y;
+            }
+
+            set
+            {
+                this.y = value;
+            }
+        }
+    }
+}
